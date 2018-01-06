68c7d2dd838f0006b2323eebb15b3e42f0c47342
Shawmiya <yoshawmi94@gmail.com>
2018-01-06 12:45:59 +0530

    aboutus  page

--- heaven/js/script.js
+++ heaven/js/script.js
@@ -89,6 +89,8 @@ $(window).on("load", function() {
     });
 
 
+
+
     /* ============ Heaven Slider ================*/
     var owl = $('.heaven-slider').owlCarousel({
         autoplay: true,
@@ -145,3 +147,24 @@ $(window).on("load", function() {
 
 
 }); /*=== Window.Load Ends Here ===*/
+
+// $(document).ready(function() {
+//
+//     $("#owl-demo").owlCarousel({
+//
+//         autoPlay: 3000, //Set AutoPlay to 3 seconds
+//
+//         items : 4,
+//         itemsDesktop : [1199,3],
+//         itemsDesktopSmall : [979,3]
+//
+//     });
+//
+// });
+
+$('.owl-carousel').owlCarousel({
+    autoplay: true,
+    center: true,
+    loop: true,
+    nav: true,
+});
